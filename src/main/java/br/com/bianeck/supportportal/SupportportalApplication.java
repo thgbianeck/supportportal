53fbb62bcc532287236e09f3a64d3f81e9ebc932
Thiago Bianeck <thiago.bianeck@viasoft.com.br>
2020-12-20 21:21:49 -0300

    Initial Commit

---
+++ src/main/java/br/com/bianeck/supportportal/SupportportalApplication.java
@@ -0,0 +1,13 @@
+package br.com.bianeck.supportportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportportalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bianeck/supportportal/SupportportalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bianeck.supportportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
