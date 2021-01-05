54c01a2d49c53726e833ac472e0a560711bce2d0
Thiago Bianeck <thiago.bianeck@viasoft.com.br>
2021-01-05 17:47:02 -0300

    Authentication Failure Listener

---
+++ src/main/java/br/com/bianeck/supportportal/listener/AuthenticationFailureListener.java
@@ -0,0 +1,25 @@
+package br.com.bianeck.supportportal.listener;
+
+import br.com.bianeck.supportportal.service.LoginAttemptService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.ExecutionException;
+
+@Component
+@RequiredArgsConstructor
+public class AuthenticationFailureListener {
+
+    private final LoginAttemptService loginAttemptService;
+
+    @EventListener
+    public void onAuthenticationFailure(AuthenticationFailureBadCredentialsEvent event) throws ExecutionException {
+        Object principal = event.getAuthentication().getPrincipal();
+        if(principal instanceof String) {
+            String username = (String) event.getAuthentication().getPrincipal();
+            loginAttemptService.addUserToLoginAttemptCache(username);
+        }
+    }
+}
