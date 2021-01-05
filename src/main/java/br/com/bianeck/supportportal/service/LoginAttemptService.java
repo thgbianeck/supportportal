d230a56b6fc4b6fcdd0c96dad17c5197aa745d4d
Thiago Bianeck <thiago.bianeck@viasoft.com.br>
2021-01-05 15:10:31 -0300

    Brute Force Attack Cache

---
+++ src/main/java/br/com/bianeck/supportportal/service/LoginAttemptService.java
@@ -0,0 +1,48 @@
+package br.com.bianeck.supportportal.service;
+
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+
+import static java.util.concurrent.TimeUnit.MINUTES;
+
+@Service
+public class LoginAttemptService {
+
+    public static final int MAXIMUM_NUMBER_OF_ATTEMPTS = 5;
+    public static final int ATTEMPT_INCREMENT = 1;
+    private LoadingCache<String, Integer> loginAttemptCache;
+
+    public LoginAttemptService() {
+        super();
+        loginAttemptCache = CacheBuilder.newBuilder()
+                .expireAfterWrite(15, MINUTES)
+                .maximumSize(100)
+                .build(
+                        new CacheLoader<String, Integer>() {
+                            @Override
+                            public Integer load(String key) throws Exception {
+                                return 0;
+                            }
+                        }
+                );
+    }
+
+    public void evictUserFromLoginAttemptCache(String username) {
+        this.loginAttemptCache.invalidate(username);
+    }
+
+    public void addUserToLoginAttemptCache(String username) throws ExecutionException {
+        int attempts = 0;
+        attempts = ATTEMPT_INCREMENT + loginAttemptCache.get(username);
+        loginAttemptCache.put(username, attempts);
+    }
+
+    public boolean hasExceededMaxAttempts(String username) throws ExecutionException {
+        return loginAttemptCache.get(username) >= MAXIMUM_NUMBER_OF_ATTEMPTS;
+    }
+}
