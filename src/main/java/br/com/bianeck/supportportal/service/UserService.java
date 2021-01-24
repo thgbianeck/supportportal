97999c4cdc1d49b727c7da0ad6d7537f3bf1b232
Thiago Bianeck <thiago.bianeck@viasoft.com.br>
2021-01-24 01:47:37 -0300

    User Service Implementation PArt 1

--- src/main/java/br/com/bianeck/supportportal/service/UserService.java
+++ src/main/java/br/com/bianeck/supportportal/service/UserService.java
@@ -1,11 +1,11 @@
 package br.com.bianeck.supportportal.service;
 
 import br.com.bianeck.supportportal.domain.User;
-import br.com.bianeck.supportportal.exception.domain.EmailExistException;
-import br.com.bianeck.supportportal.exception.domain.UserNotFoundException;
-import br.com.bianeck.supportportal.exception.domain.UsernameExistException;
+import br.com.bianeck.supportportal.exception.domain.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.mail.MessagingException;
+import java.io.IOException;
 import java.util.List;
 
 public interface UserService {
@@ -17,4 +17,14 @@ public interface UserService {
     User findUserByUsername(String username);
 
     User findUserByEmail(String email);
+
+    User addNewUser(String firstName, String lastName, String username, String email, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, NotAnImageFileException;
+
+    User updateUser(String currentUsername, String newFirstName, String newLastName, String newUsername, String newEmail, String role, boolean isNonLocked, boolean isActive, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, NotAnImageFileException;
+
+    void deleteUser(String username) throws IOException;
+
+    void resetPassword(String email) throws MessagingException, EmailNotFoundException;
+
+    User updateProfileImage(String username, MultipartFile profileImage) throws UserNotFoundException, UsernameExistException, EmailExistException, IOException, NotAnImageFileException;
 }
